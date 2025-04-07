fa2dec76d18dced43b0f037a95c4dbd93e1e533d
Paulo Rogério <526290+progerio@users.noreply.github.com>
2025-04-07 09:38:02 -0300

    Update 1_users.up.sql

--- migrations/1_users.up.sql
+++ migrations/1_users.up.sql
@@ -1,4 +1,4 @@
 CREATE TABLE IF NOT EXISTS users (
-                                     id SERIAL PRIMARY KEY,
-                                     name VARCHAR NOT NULL
+  id SERIAL PRIMARY KEY,
+  name VARCHAR NOT NULL
 );
